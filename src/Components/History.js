fdca2d2ccf297c6824995f97fbdce18fd6e1ce2c
tarun954 <tarunnaidu43637@gmail.com>
2022-05-18 18:36:22 +0530

    total project

---
+++ src/Components/History.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function History({history,moveTo,currentMove}) {
+    return (
+        <div className="history-wrapper">
+            <ul className="history">
+            {history.map((_,move) => {
+                    return(
+                        <li key={move}>
+                            <button 
+                            className={`btn-move ${move === currentMove ? 'active' : ''}`}
+                            style={{ 
+                            fontWeight: move === currentMove ? 'bold' : 'normal',
+                            }} type="submit"  onClick={() => {
+                                moveTo(move);
+                            }}>{move === 0 ? 'Go to game start' : `Go to move #${move}`}</button>
+                        </li>
+                    );
+               })}
+        </ul>
+
+        </div>
+
+    )
+}
+
+export default History
---
+++ src/Components/StatusMessages.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const StatusMessages = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+
+  return (
+    <div className="status-message">
+      {winner && <>
+      winner is {''}
+      <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+        {winner}
+      </span>
+      </>}
+      {!winner &&
+        !noMovesLeft &&
+        <>
+        Next player is{''}
+         <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+         {current.isXNext ? 'X' : 'O'}{''} 
+        </span>
+        </>}
+      {!winner && noMovesLeft &&  
+      <>
+      <span className="text-green">
+        X</span> and <span className="text-orange">O</span> <span className="reset">game tied</span>
+        </> }
+    </div>
+  );
+};
+
+export default StatusMessages;
